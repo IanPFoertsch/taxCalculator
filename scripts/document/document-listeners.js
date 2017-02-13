cfaa69141e46e6a53ebf7a7dc134d653d4341acf
Ian Foertsch <IanPFo@gmail.com>
2017-02-13 07:43:16 -0500

    added gruntfile

--- scripts/document/document-listeners.js
+++ scripts/document/document-listeners.js
@@ -1,10 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var taxes = [
+    federalTaxes,
+    socialSecurityWithholding,
+    medicareWithholding
+  ];
+
+  function populateTable(table) {
+    _.forEach(taxes, function(tax) {
+      row = table.insertRow();
+      nameCell = row.insertCell(0);
+      nameCell.innerHTML = tax.name;
+      //insert a value cell
+      row.insertCell(1);
+    });
+  }
+  populateTable(document.getElementById('tax-table'));
+
+  function clearTableValues(table) {
+    _.forEach(table.rows, (function(row) {
+      row.cells[1].innerHTML = "";
+    }));
+
+    return table;
+  }
+
   document.getElementById("calculateButton").addEventListener("click", function(){
-    var val = document.getElementById("incomeInput").value;
-    var calculator = new Calculator();
-    var table = document.querySelectorAll('.tax-table');
-    document.getElementById("federalIncomeTaxAmount").innerHTML = calculator.calculateTaxes(val);
+    var grossIncome = document.getElementById("grossIncome").value;
+
+
+    table = clearTableValues(document.getElementById('tax-table'));
+
+    _.forEach(taxes, function(tax, index) {
+      row = table.rows[index];
+      valueCell = row.cells[1];
+      valueCell.innerHTML = tax(grossIncome);
+    });
   });
-
-
 });
