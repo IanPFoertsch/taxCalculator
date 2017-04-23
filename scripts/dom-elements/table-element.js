05d3ab4e649d287ee2b6ff6eb4a8b354ea2a6451
Ian Foertsch <IanPFo@gmail.com>
2017-04-23 17:59:18 -0400

    programmatic table inheritance

--- scripts/dom-elements/table-element.js
+++ scripts/dom-elements/table-element.js
@@ -1,11 +1,49 @@
-function TableElement() {
-  this.prepare = function(parentIdentifier, cssClasses) {
-    var parent = document.querySelector(parentIdentifier);
+function TableElement(config, parentIdentifier) {
+  this.config = config;
+  this.parentIdentifier = parentIdentifier;
+  this.identifier = '.' + config.cssClasses;
 
-    var child = document.createElement("Table");
+  this.titleRow = new TitleRowElement(config.titleRow, this.identifier);
+}
 
-    child.className += cssClasses;
+TableElement.prototype.prepare = function() {
+  var parent = document.querySelector(this.parentIdentifier);
+  var child = document.createElement("Table");
 
-    parent.appendChild(child);
-  };
+  child.className += this.config.cssClasses;
+  parent.appendChild(child);
+
+  this.prepareChildren();
+};
+
+
+TableElement.prototype.prepareChildren = function() {
+  console.log("this was called");
+  this.titleRow.prepare();
+};
+
+function InputTableElement(config, parentIdentifier) {
+  TableElement.call(this, config, parentIdentifier);
+  var self = this;
+
+  this.rows = _.map(config.rows, function(row) {
+    return new InputRowElement(row, self.identifier);
+  });
+}
+
+InputTableElement.prototype = Object.create(TableElement.prototype);
+
+InputTableElement.prototype.prepareChildren = function() {
+  TableElement.prototype.prepareChildren.call(this);
+  console.log("this was also called");
+  _.each(this.rows, function(row) {
+    row.prepare();
+  });
+};
+
+
+function OutputTableElement(config, parentIdentifier) {
+  TableElement.call(this, config, parentIdentifier);
+
+  
 }
--- scripts/dom-elements/title-row-element.js
+++ scripts/dom-elements/title-row-element.js
@@ -1,14 +1,15 @@
-function TitleRowElement(label) {
-  this.label = label;
+function TitleRowElement(config, parentIdentifier) {
+  this.title = config.title;
+  this.parentIdentifier = parentIdentifier;
 
-  this.prepare = function(parentIdentifier) {
+  this.prepare = function() {
     //TODO: this is also very similar logic between classes. figure out
     //how inheritance works in javascript and use that
-    var parent = document.querySelector(parentIdentifier);
+    var parent = document.querySelector(this.parentIdentifier);
 
     var div = document.createElement('Div');
     div.className += "title-row";
-    div.innerText = this.label;
+    div.innerText = this.title;
 
     parent.appendChild(div);
   };
