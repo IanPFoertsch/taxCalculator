ade7f1dae210fd62371625d3218b2ef1de2c909c
Ian Foertsch <IanPFo@gmail.com>
2017-08-02 08:00:39 -0400

    ChartJS data conversion for stacked bar charts

--- spec/jasmine_examples/adapters/chart-js-adapter-spec.js
+++ spec/jasmine_examples/adapters/chart-js-adapter-spec.js
@@ -2,11 +2,13 @@
 describe('ChartJSAdapter', function() {
   var input = [{ a: 1, b: 2 }, { a: 2, b: 3 }];
   var result;
-  beforeEach(() => {
-    result = ChartJSAdapter.cashFlowConversion(input);
-  });
 
-  describe('cashFlowConversion', () => {
+
+  describe('lineChartConversion', () => {
+    beforeEach(() => {
+      result = ChartJSAdapter.lineChartConversion(input);
+    });
+
     it('should convert to a series of data for each variable', () => {
       var expectedKeys = Object.keys(input[0]);
       expectedKeys.forEach((key) => {
@@ -17,6 +19,31 @@ describe('ChartJSAdapter', function() {
     it('should format the data to {x: _, y: _} format', () => {
       expect(result.a[0]).toEqual({ x:0, y: 1 });
       expect(result.a[1]).toEqual({ x:1, y: 2 });
+    });
+  });
+
+  describe('stackedBarChartConversion', () => {
+    beforeEach(() => {
+      result = ChartJSAdapter.stackedBarChartConversion(input);
+    });
+
+    it('should have an array of labels for each variable', () => {
+      expect(result.labels).toEqual(['a', 'b']);
+    });
+
+    it('should have a background color for each value', () => {
+      expect(result.datasets[0].backgroundColor).not.toBeUndefined();
+      expect(result.datasets[1].backgroundColor).not.toBeUndefined();
+    });
+
+    it('should have a stack number for each value', () => {
+      expect(result.datasets[0].stack).not.toBeUndefined();
+      expect(result.datasets[1].stack).not.toBeUndefined();
+    });
+
+    it('should have a data field for each value', () => {
+      expect(result.datasets[0].data).toEqual([1, 2]);
+      expect(result.datasets[1].data).toEqual([2, 3]);
     });
   });
 });
