1928367c530b60585cdc9ecd3f73596fbdc564ab
Ian Foertsch <IanPFo@gmail.com>
2018-04-09 07:48:59 -0400

    Disabled missing semicolon warning in karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,8 @@ module.exports = function(grunt) {
         globals: {
           jQuery: true
         },
-        'esversion': 6
+        'esversion': 6,
+        'asi': true
       }
     },
     watch: {
